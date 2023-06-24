af0c868c44b05fb921d0955bee2b2321b5b779d6
BahmatowDanil <mr.melon666@gmail.com>
2023-06-25 03:37:20 +0400

    Initial

---
+++ main.cpp
@@ -0,0 +1,26 @@
+
+#include <bit>
+#include <iostream>
+#include <string>
+#include <atomic>
+#include <thread>
+
+#include <list>
+
+std::atomic<int> flag;
+std::list<std::thread> threadList; 
+
+int main(){
+    std::cout << "hello" << std::endl;
+    while(flag < 10)
+    {
+        threadList.push_back(std::thread ([]{
+            std::cout << flag++ << std::endl;
+        }));
+    }
+    for(auto &&thread : threadList)
+    {
+        thread.join();
+    }
+    return 0;
+}
